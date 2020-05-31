4a77221fdbd14d5de55d14fd652c789cf968f906
Al8xandra <rogachs2001@gmail.com>
2020-05-31 20:58:01 +0300

    Add models

---
+++ Course_6/DAL/Models/Role.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DAL.Models
+{
+    /// <summary>
+    /// Model of Role
+    /// </summary>
+    [Table("Roles")]
+    public class Role
+    {
+        /// <summary>
+        /// Role identifier
+        /// </summary>
+        public int RoleId { get; set; }
+
+        /// <summary>
+        /// Role name
+        /// </summary>
+        public string RoleName { get; set; }
+    }
+}
---
+++ Course_6/DAL/Models/Service.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DAL.Models
+{
+    /// <summary>
+    /// Model of Service
+    /// </summary>
+    [Table("Services")]
+    public class Service
+    {
+        /// <summary>
+        /// Service identifier
+        /// </summary>
+        [Key]
+        public int ServiceId { get; set; }
+
+        /// <summary>
+        /// Service name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Service type
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Service price
+        /// </summary>
+        public string Price { get; set; }
+
+        /// <summary>
+        /// Collection of users who bought this service
+        /// </summary>
+        public virtual ICollection<User> UsersWhoBought { get; set; }
+    }
+}
