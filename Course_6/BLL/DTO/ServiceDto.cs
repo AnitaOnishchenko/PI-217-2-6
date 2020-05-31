0ece3fe3848cc377813bebd439b556b5ed800849
Al8xandra <rogachs2001@gmail.com>
2020-05-31 21:31:53 +0300

    Add Data transfer objects

---
+++ Course_6/BLL/DTO/ServiceDto.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using DAL.Models;
+
+namespace BLL.DTO
+{
+    /// <summary>
+    /// Data transfer object for <see cref="Service"/>
+    /// </summary>
+    public class ServiceDto
+    {
+        /// <summary>
+        /// Service identifier
+        /// </summary>
+        public int ServiceId { get; set; }
+
+        /// <summary>
+        /// Service name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Service type
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Service price
+        /// </summary>
+        public string Price { get; set; }
+
+        /// <summary>
+        /// Collection of users who bought this service
+        /// </summary>
+        public virtual ICollection<User> UsersWhoBought { get; set; }
+    }
+}
---
+++ Course_6/BLL/DTO/UserDto.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using DAL.Models;
+
+namespace BLL.DTO
+{
+    /// <summary>
+    /// Data transfer object for <see cref="User"/>
+    /// </summary>
+    public class UserDto
+    {
+        /// <summary>
+        /// User identifier
+        /// </summary>
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// User's name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// User's login
+        /// </summary>
+        public string Login { get; set; }
+
+        /// <summary>
+        /// User's password
+        /// </summary>
+        public string Pass { get; set; }
+
+        /// <summary>
+        /// User's role id
+        /// </summary>
+        public int RoleId { get; set; }
+
+        /// <summary>
+        /// User's role
+        /// </summary>
+        public virtual RoleDto Role { get; set; }
+
+        /// <summary>
+        /// Collection of services which were bought by this user
+        /// </summary>
+        public virtual ICollection<ServiceDto> Services { get; set; }
+    }
+}
